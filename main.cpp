13d5c63f98bcb5bd8413d783112afe9d746f9ae2
Kogoon <gjs712@naver.com>
2020-04-12 05:50:08 -0400

    Modified main.cpp ing...

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <libnet.h>
 #include <netinet/in.h> //in_addr
 
+#define ETHERTYPE_IP    0x0800
 
 struct ethernet_str {
     uint8_t ether_dhost[6];
@@ -20,7 +21,7 @@ struct ethernet_str {
 };
 
 struct ipv4_str {
-    uint8_t ip_ver:4, ip_hdrlen:4; // version 4, headerlength
+    uint8_t ip_ver_hdlen; // version 4, headerlength
     uint16_t ip_total_len;
     uint16_t ip_id;
     uint16_t ip_flag;
@@ -35,33 +36,26 @@ struct ipv4_str {
 struct tcp_str {
     uint16_t port_src;
     uint16_t port_dst;
+    uint32_t tcp_seq;
+    uint32_t tcp_ack;
+    uint8_t tcp_offR;
+    uint8_t tcp_flags;
+    uint16_t tcp_win;
+    uint16_t tcp_sum;
+    uint16_t tcp_up;
 };
 
-void show_mac(struct ethernet_str *eth_h) {
-    printf("Destination MAC : ");
+void print_mac(uint8_t const ether_mac[6]) {
     for (int i=0; i<6; i++) {
-        printf("%02X", eth_h->ether_dhost[i]);
-        if(i<5) printf(":");
-    }
-    printf("\n");
-    printf("Source MAC : ");
-    for (int i=0; i<6; i++) {
-        printf("%02X", eth_h->ether_shost[i]);
+        printf("%02X", ether_mac[i]);
         if(i<5) printf(":");
     }
     printf("\n");
 }
 
-void show_ip(struct ipv4_str *ip4_h) {
-    printf("Source IP : ");
+void print_ip(uint8_t const ip4[4]) {
     for (int i=0; i<4; i++) {
-        printf("%u", ip4_h->ip_src[i]);
-        if(i<3) printf(".");
-    }
-    printf("\n");
-    printf("Destination IP : ");
-    for (int i=0; i<4; i++){
-        printf("%u", ip4_h->ip_dst[i]);
+        printf("%u", ip4[i]);
         if(i<3) printf(".");
     }
     printf("\n");
@@ -102,38 +96,43 @@ int main(int argc, char* argv[]) {
 
         // requirement libnet-headers.h
         struct ethernet_str *eth_h = (struct ethernet_str*)packet;
-        
-        if (htons(eth_h->type) == 0x0800) {
-            struct ipv4_str *ip4_h = (struct ipv4_str*)(packet+14);
+        if (htons(eth_h->type) == ETHERTYPE_IP) {
+            struct ipv4_str *ip4_h = (struct ipv4_str*)(packet+sizeof(ethernet_str));
+            int ip_hlen = (ip4_h->ip_ver_hdlen & 0x0F)*4;
             // condition(?) ip header -> tcp header? 0x06 protocol. only tcp packet.
+            //printf("%u", ip4_h->ip_ver_hdlen & 0x0F);
             if ((ip4_h->ip_p) == 0x06) {
-                struct tcp_str *tcp_h =(struct tcp_str*)(packet+((ip4_h->ip_hdrlen)*4));
+                struct tcp_str *tcp_h =(struct tcp_str*)(packet+ip_hlen+sizeof(ethernet_str));
+                int tcp_hlen = ((tcp_h->tcp_offR & 0xF0)>>4)*4;
+                //printf("0x%x %d", ((tcp_h->tcp_offR & 0xF0) >>4), tcp_hlen);
                 // src mac, dst mac / eth_h->ether_shost, eth_h->ether_dhost
                 printf("\n------------------------------------------------------\n");
-                /*
-                printf("destination mac : %02X:%02X:%02X:%02X:%02X:%02X\n", eth_h->ether_dhost[0], eth_h->ether_dhost[1], eth_h->ether_dhost[2],
-                        eth_h->ether_dhost[3], eth_h->ether_dhost[4], eth_h->ether_dhost[5]);
-                printf("source mac      : %02X:%02X:%02X:%02X:%02X:%02X\n", eth_h->ether_shost[0], eth_h->ether_shost[1], eth_h->ether_shost[2],
-                        eth_h->ether_shost[3], eth_h->ether_shost[4], eth_h->ether_shost[5]);
-                //printf("%04X\n", ntohs(eth_h->type));
-                */
-                show_mac(eth_h);
-    
+                printf("Destination MAC : ");
+                print_mac(eth_h->ether_dhost);
+                printf("Source MAC : ");
+                print_mac(eth_h->ether_shost);
+
                 // src ip, dst ip / ip4_h->ip_src, ip4_h->ip_dst
-                //printf("\n------------------------------------------------------\n");
-                //printf("Source IP      : %s\n", inet_ntoa(ip4_h->ip_src));
-                //printf("Destination IP : %s\n", inet_ntoa(ip4_h->ip_dst));
-                //printf("Source IP      : %u.%u.%u.%u\n", ip4_h->ip_src[0], ip4_h->ip_src[1], ip4_h->ip_src[2], ip4_h->ip_src[3]);
-                //printf("Destination IP : %u.%u.%u.%u\n", ip4_h->ip_dst[0], ip4_h->ip_dst[1], ip4_h->ip_dst[2], ip4_h->ip_dst[3]);
-                show_ip(ip4_h);
+                printf("Source IP : ");
+                print_ip(ip4_h->ip_src);
+                printf("Destination IP : ");
+                print_ip(ip4_h->ip_dst);
 
                 // src port_n, dst port_n / posrt_src, tcp_h->port_dst
                 //printf("\n------------------------------------------------------\n");
-                printf("%0004X\n", tcp_h->port_src);
-                printf("Source Port Number      : %u\n", tcp_h->port_src);
-                printf("Destination Port Number : %u\n", tcp_h->port_dst);
-    
-                //for (int i=0; i<16; i++) { printf("%02X "); }
+                //printf("%0004X\n", tcp_h->port_src);
+                printf("Source Port Number      : %u\n", ntohs(tcp_h->port_src));
+                printf("Destination Port Number : %u\n", ntohs(tcp_h->port_dst));
+
+                const u_char *data_payload = packet + sizeof(ethernet_str) + ip_hlen + tcp_hlen;
+                int data_len = ntohs(ip4_h->ip_total_len) - tcp_hlen - ip_hlen;
+                if (data_len > 0) {
+                    printf(" Data \n ->");
+                    for (int i=0; i<16; i++) {
+                        printf("%02X ", *data_payload);
+                        data_payload++;
+                    }
+                }
 
             }
         }
